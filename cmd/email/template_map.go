27fc086ab39bc40606983bb67669148fc75901c9
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2024-11-10 18:23:55 -0500

    fix imports

--- cmd/email/template_map.go
+++ cmd/email/template_map.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -12,20 +13,26 @@ import (
 var templates = loadTemplates()
 
 func loadTemplates() map[string]*template.Template {
-	tmpls := make(map[string]*template.Template)
-	files, err := filepath.Glob("cmd/email/templates/**/*.html")
+	tmplPath, exists := os.LookupEnv("TEMPLATE_PATH")
+	if !exists {
+		tmplPath = "cmd/email/templates"
+	}
+
+	files, err := filepath.Glob(filepath.Join(tmplPath, "**/*.html"))
 	if err != nil {
 		log.Fatal("Failed to load email templates:", err)
 	}
+
+	tmpls := make(map[string]*template.Template)
 	for _, file := range files {
-		name := strings.ReplaceAll(file[len("cmd/email/templates/"):], `\`, `/`)
+		name := strings.ReplaceAll(file[len(tmplPath):], `\`, `/`)
 		tmpls[name] = template.Must(template.ParseFiles(file))
 	}
+
 	return tmpls
 }
 
 func RenderTemplate(templatePath string, data interface{}) (string, error) {
-	fmt.Println("TEMPLATES FOUND: ", len(templates), templates)
 	tmpl, ok := templates[templatePath]
 	if !ok {
 		return "", fmt.Errorf("template %s not found", templatePath)
